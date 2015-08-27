b552421f5e01fccca56607e7cde4d2c2ce1cae4b
Mike Zaccardo <mike.zaccardo@cloudsoftcorp.com>
2015-08-27 11:16:12 +0100

    Update server backend to match REST API changes

--- rest-server/src/main/java/org/brooklyncentral/catalog/rest/resources/MainResource.java
+++ rest-server/src/main/java/org/brooklyncentral/catalog/rest/resources/MainResource.java
@@ -1,56 +1,34 @@
 package org.brooklyncentral.catalog.rest.resources;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
-import org.apache.brooklyn.util.collections.MutableMap;
-import org.brooklyncentral.catalog.model.Repository;
-import org.brooklyncentral.catalog.model.RepositoryLinkList;
+import org.brooklyncentral.catalog.dto.CatalogItem;
 import org.brooklyncentral.catalog.rest.api.MainApi;
 import org.brooklyncentral.catalog.rest.server.CatalogServerState;
 
 public class MainResource implements MainApi {
 
     @Context ServletContext servletContext;
-    
-    @Override
-    public List<Map<String,String>> listRepositories(String regex, String fragment) {
-        // TODO support filtering by regex and fragment (currently parameters are ignored)
-        RepositoryLinkList rl =
-            CatalogServerState.getInstance(servletContext).getRepositories();
-            //RepositoriesList.newInstance(CatalogServerConfig.retrieve(servletContext));
-        return rl.asList();
-    }
-        
-    @Override
-    public Map<String,Object> getRepo(String ownerName, String repoName) {
-        Repository repo = CatalogServerState.getInstance(servletContext).getRepository(ownerName, repoName);
-        if (repo==null) {
-            throw new WebApplicationException(Response.status(404).build());
-        }
-        MutableMap<String,Object> result = MutableMap.<String,Object>copyOf(repo.getLink().asMap());
-        result.put("bomText", repo.getCatalogBomString());
-        result.put("bomMap", repo.getCatalogBomYaml());
-        return result;
-    }
-    
-    @Override
-    public String getRepoCatalog(String ownerName, String repoName) {
-        Repository repo = CatalogServerState.getInstance(servletContext).getRepository(ownerName, repoName);
-        if (repo==null) {
-            throw new WebApplicationException(Response.status(404).build());
-        }
-        
-//        url:null,
-//        name:null,
-//        owner:null
 
-        return repo.getCatalogBomString();
+    // TODO Support filtering by regex and fragment (currently parameters are ignored)
+    @Override
+    public List<CatalogItem> listCatalogItems(String regex, String fragment) {
+        return CatalogServerState.getInstance(servletContext).getCatalogItems();
+    }
+
+    @Override
+    public CatalogItem getCatalogItem(String ownerName, String repoName) {
+        CatalogItem catalogItem = CatalogServerState.getInstance(servletContext).getCatalogItem(ownerName, repoName);
+        if (catalogItem == null) {
+            throw new WebApplicationException(Response.status(404).build());
+        }
+
+        return catalogItem;
     }
 
 }
--- rest-server/src/main/java/org/brooklyncentral/catalog/rest/server/CatalogServerState.java
+++ rest-server/src/main/java/org/brooklyncentral/catalog/rest/server/CatalogServerState.java
@@ -1,84 +1,59 @@
 package org.brooklyncentral.catalog.rest.server;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
 
-import org.apache.brooklyn.util.collections.MutableMap;
-import org.brooklyncentral.catalog.model.Repository;
-import org.brooklyncentral.catalog.model.RepositoryLink;
-import org.brooklyncentral.catalog.model.RepositoryLinkList;
+import org.brooklyncentral.catalog.dto.CatalogItem;
+import org.brooklyncentral.catalog.scrape.CatalogItemScraper;
+
+import com.google.common.collect.ImmutableList;
 
 public class CatalogServerState {
 
-    RepositoryLinkList repos;
-    Map<String,Repository> cache = MutableMap.of();
-    
-    private CatalogServerState() {}
-    
-    public static CatalogServerState newInstance(RepositoryLinkList repos) {
-        CatalogServerState result = new CatalogServerState();
-        result.repos = repos;
-        return result;
+    private final Map<String, CatalogItem> catalogItems;
+
+    private CatalogServerState(Map<String, CatalogItem> catalogItems) {
+        this.catalogItems = catalogItems;
     }
-    
+
+    public static CatalogServerState newInstance(Map<String, CatalogItem> catalogItems) {
+        return new CatalogServerState(catalogItems);
+    }
+
+    private static CatalogServerState newInstance(ServletContext context, CatalogServerConfig config) {
+        CatalogServerState result = (CatalogServerState) context.getAttribute(CatalogServerState.class.getName());
+        if (result!=null) return result;
+
+        Map<String, CatalogItem> catalogItems = CatalogItemScraper.scrapeCatalogItems(config.getRepositoriesUrl());
+        return newInstance(catalogItems);
+    }
+
     public static CatalogServerState getInstance(ServletContext context) {
         CatalogServerState result = (CatalogServerState) context.getAttribute(CatalogServerState.class.getName());
         if (result!=null) return result;
+
         CatalogServerConfig config = CatalogServerConfig.retrieve(context);
         if (config==null) {
-            throw new IllegalStateException(CatalogServerConfig.class.getName()+" not set on "+context);
+            throw new IllegalStateException(CatalogServerConfig.class.getName() + " not set on " + context);
         }
+
         synchronized (config) {
             result = newInstance(context, config);
             context.setAttribute(CatalogServerState.class.getName(), result);
         }
+
         return result;
     }
 
-    private static CatalogServerState newInstance(ServletContext context, CatalogServerConfig config) {
-        CatalogServerState result;
-        result = (CatalogServerState) context.getAttribute(CatalogServerState.class.getName());
-        if (result!=null) return result;
-        result = newInstance(RepositoryLinkList.newInstance( config ));
-        return result;
+    public List<CatalogItem> getCatalogItems() {
+        return ImmutableList.copyOf(catalogItems.values());
     }
 
-    public RepositoryLinkList getRepositories() {
-        return repos;
+    public CatalogItem getCatalogItem(String ownerName, String repoName) {
+        String token = ownerName + "/" + repoName;
+        return catalogItems.get(token);
     }
 
-    public Repository getRepository(String ownerName, String repoName) {
-        String token = ownerName+"/"+repoName;
-        Repository r = cache.get(token);
-        if (r!=null) return r;
-        
-        for (Map<String,String> rm : repos.asList()) {
-            RepositoryLink rl = RepositoryLink.newInstance(rm);
-            if (rl.getOwner().equals(ownerName) && rl.getShortName().equals(repoName)) {
-                r = loadAndAddToCache(rl);
-                cache.put(token, r);
-                return r;
-            }
-        }
-        
-        return null;
-    }
-
-    private Repository loadAndAddToCache(RepositoryLink rl) {
-        // load it
-        Repository r = Repository.newInstance(rl);
-        // now cache it (unless someone else has also loaded it meanwhile)
-        synchronized (cache) {
-            Repository r0 = cache.get(rl.getOwnerSlashName());
-            if (r0!=null) {
-                // don't add to cache, someone else populated it at the same time;
-                // ensure everyone sees same object (probably doesnt' matter, but slightly cleaner)
-                return r0;
-            }
-            cache.put(r.getOwnerSlashName(), r);
-            return r;
-        }
-    }
-    
 }
